8696d0ed10efca2b6d1016aa337272c396d88cc9
Lewis Jardine <lewisjardine777@gmail.com>
2022-10-07 08:47:01 +0100

    Init Commit

---
+++ detect_from_image.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+import face_recognition
+
+# Load images
+image_dir = "./input_faces/"
+obama_image = cv2.imread(image_dir + 'barack-obama-1.jpg')
+biden_image = cv2.imread(image_dir + 'joe-biden-1.jpg')
+unkown_image = cv2.imread(image_dir + 'biden-obama-1.jpeg')
+
+# Encode known faces
+obama_image_encoding = face_recognition.face_encodings(obama_image)[0]
+biden_image_encoding = face_recognition.face_encodings(biden_image)[0]
+
+# Create arrays of known encoded faces and names
+known_face_encodings = [
+  obama_image_encoding, 
+  biden_image_encoding
+]
+known_face_names = [
+  "Barack Obama",
+  "Joe Biden"
+]
+
+face_names = []
+# Find location of faces in unkown image, encode those
+unknown_face_locations = face_recognition.face_locations(unkown_image)
+unkown_image_encoding = face_recognition.face_encodings(unkown_image, unknown_face_locations)
+
+# Iterate through encoded faces in unkown image
+for face_encoding in unkown_image_encoding:
+
+  # Find if face is a match to known faces
+  matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+  name = "unkown"
+
+  # Find known face with shortest distance to unkown one
+  face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+  best_match_index = np.argmin(face_distances)
+  if matches[best_match_index]:
+    name = known_face_names[best_match_index]
+
+  # Append name of face (either found or unkown) to array
+  face_names.append(name)
+
+# Draw name and bounding boxes onto image
+for (top, right, bottom, left), name in zip(unknown_face_locations, face_names):
+
+  # Draw bounding box around face
+  cv2.rectangle(unkown_image, (left, top), (right, bottom), (0, 255, 0), 2)
+
+  # Draw label with name below corrosponding face
+  cv2.rectangle(unkown_image, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+  font = cv2.FONT_HERSHEY_DUPLEX
+  cv2.putText(unkown_image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+# Display image w/ bounding boxes over faces
+cv2.imshow("image", unkown_image)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
---
+++ detect_from_webcam.py
@@ -0,0 +1,106 @@
+from genericpath import isfile
+import numpy as np
+import cv2
+import face_recognition
+import os
+
+# Load trained images
+trained_path = "./trained_images/"
+trained_images = []
+trained_images_names = []
+
+# Path to place unkown faces
+unkown_path = "./unkown_faces/"
+
+# Loop through all image paths in trained images folder
+for image_path in os.listdir(trained_path):
+
+
+  # Create full input path and read then encode image
+  input_path = os.path.join(trained_path, image_path)
+  image = cv2.imread(input_path)
+  encoded = face_recognition.face_encodings(image)[0]
+  trained_images.append(encoded)
+
+  # Add name to face
+  trained_images_names.append(image_path)
+
+
+# Start webcam, run facial rec every n frame
+vid = cv2.VideoCapture(0)
+n = 1
+frame_count = 0
+unkown_count = 1
+
+while(True):
+
+  if frame_count % n == 0:
+    face_names = []
+
+    # Capture vid frame by frame
+    ret, frame = vid.read()
+
+    # Find location of faces in frame, encode those
+    unknown_face_locations = face_recognition.face_locations(frame)
+    unkown_image_encoding = face_recognition.face_encodings(frame, unknown_face_locations)
+
+    # Iterate through encoded faces in unkown image
+    for face_encoding in unkown_image_encoding:
+
+      # Find if face is a match to known faces
+      matches = face_recognition.compare_faces(trained_images, face_encoding)
+      name = "unkown"
+
+      # Find known face with shortest distance to unkown one
+      face_distances = face_recognition.face_distance(trained_images, face_encoding)
+      best_match_index = np.argmin(face_distances)
+      if matches[best_match_index]:
+        name = trained_images_names[best_match_index]
+
+      # Append name of face (either found or unkown) to array
+      face_names.append(name)
+
+      # Draw name and bounding boxes onto image
+      for (top, right, bottom, left), name in zip(unknown_face_locations, face_names):
+
+        if name == "unkown":
+
+          # Crop image to just face
+          crop_face = frame[top: bottom, left: right]
+
+          # Encode unknown face
+          unkown_face_encoded = face_recognition.face_encodings(crop_face)
+
+          # If high enough conf there is a face, grab first face in array
+          if unkown_face_encoded:
+
+            unkown_face_encoded = unkown_face_encoded[0]
+
+            # Get file name of unkown face
+            unkown_face_name = name + str(unkown_count) + '.jpg'
+            unkown_count += 1
+
+            # Encode face
+            trained_images.append(unkown_face_encoded)
+            trained_images_names.append(unkown_face_name)
+
+            # Save face to file system
+            unkown_face_path = os.path.join(unkown_path, unkown_face_name)
+            cv2.imwrite(unkown_face_path, crop_face)
+
+        # Draw bounding box around face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+        # Draw label with name below corrosponding face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display image w/ bounding boxes over faces
+    cv2.imshow("image", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
+# After the loop release the cap object
+vid.release()
+cv2.destroyAllWindows()
